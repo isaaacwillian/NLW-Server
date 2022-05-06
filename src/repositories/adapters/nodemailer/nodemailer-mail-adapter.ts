b2183340ebae00956191b152e3a1f9b695828c9d
Isaac Willian <isaaacwillian@gmail.com>
2022-05-06 15:34:40 -0300

    Class 3: Applied SOLID concepts

---
+++ src/repositories/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer";
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "fe7dfbf416964e",
+    pass: "c0a014184a22c9",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Isaac Willian <isaaacwillian@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
